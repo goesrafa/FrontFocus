80a0af2ce245ad6be5471580a507a1303470ac70
goesrafa <rafa.goes.2012@gmail.com>
2022-04-16 00:56:11 -0300

    Criação dos botões estilizados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,8 @@ import styles from '../../styles/home.module.scss';
 
 import logoImg from '../../public/logo3.png'
 
-import {Input} from '../components/ui/Input'
+import { Input } from '../components/ui/Input'
+import { Button } from '../components/ui/Button'
 
 export default function Home() {
   return (
@@ -13,14 +14,17 @@ export default function Home() {
         <title>FocusCourses - Bem vindo ao login!</title>
       </Head>
       <div className={styles.containerCenter}>
-          <Image src={logoImg} alt="Logo FocusCourse" />
+        <Image src={logoImg} alt="Logo FocusCourse" />
 
-          <div className={styles.login}>
-              <form>
-                <Input placeholder='Digite seu E-mail' type="text" />
-                <Input placeholder='Digite sua senha' type="password"/>
-              </form>
-          </div>
+        <div className={styles.login}>
+          <form>
+            <Input placeholder='Digite seu E-mail' type="text" />
+            <Input placeholder='Digite sua senha' type="password" />
+            <Button type="submit" loading={false}>
+              Acessar
+            </Button>
+          </form>
+        </div>
       </div>
     </>
 
